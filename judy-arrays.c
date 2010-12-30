d8c790df7d20515cc67cb185fbc67233f34d4657
Jan <github@geheimwerk.de>
2010-12-30 22:04:55 +0100

    Increasing coverage of 64-bit detection even further.

--- judy-arrays.c
+++ judy-arrays.c
@@ -60,6 +60,8 @@ typedef uint32_t uint;
 
 #if defined(__LP64__) || \
 	defined(__x86_64__) || \
+	defined(__amd64__) || \
+	defined(_WIN64) || \
 	defined(__sparc64__) || \
 	defined(__arch64__) || \
 	defined(__powerpc64__) || \
