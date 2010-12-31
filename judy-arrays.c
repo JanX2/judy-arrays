b66e0e5acd733228a546169517c78a3b7f08b17e
Jan <github@geheimwerk.de>
2010-12-31 23:15:51 +0100

    Removing dependency on stdint.h and, for Windows, on inttypes.h

--- judy-arrays.c
+++ judy-arrays.c
@@ -31,8 +31,17 @@
 	#include <malloc.h>
 #endif
 #include <memory.h>
-#include <stdint.h>
-#include <inttypes.h>
+
+#if defined(WIN32)
+	typedef unsigned char         uint8_t;
+	typedef unsigned short       uint16_t;
+	typedef unsigned int         uint32_t;
+	typedef unsigned long long   uint64_t;
+	#define PRIu32	"u"
+	#define PRIu64	"llu"
+#else
+	#include <inttypes.h>
+#endif
 
 #ifdef linux
 	#include <endian.h>
