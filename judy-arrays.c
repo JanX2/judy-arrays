719c7820abd2b7b33aa0462417cc5eca158f96e3
Jan <github@geheimwerk.de>
2011-01-04 13:47:50 +0100

    Switching to more general solution for missing C99 support.

--- judy-arrays.c
+++ judy-arrays.c
@@ -30,15 +30,15 @@
 #include <memory.h>
 #include <limits.h>
 
-#if defined(_WIN32)
+#if __STDC_VERSION__ >= 199901L
+	#include <inttypes.h>
+#else
 	typedef unsigned char         uint8_t;
 	typedef unsigned short       uint16_t;
 	typedef unsigned int         uint32_t;
 	typedef unsigned long long   uint64_t;
 	#define PRIu32	"u"
 	#define PRIu64	"llu"
-#else
-	#include <inttypes.h>
 #endif
 
 #ifdef linux
